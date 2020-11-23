d30302f5d5aa3a4397a9730fd821a49bd4e6175b
Shuhua <nus.gao.shuhua@gmail.com>
2020-11-23 19:36:35 +0800

    Editor and File viewmodel (part 4)

---
+++ Notepad/WPFNotepad/ViewModels/FileViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using WPFNotepad.Models;
+
+namespace WPFNotepad.ViewModels
+{
+    public class FileViewModel
+    {
+        public DocumentModel Document { get; private set; }
+
+        public FileViewModel(DocumentModel document)
+        {
+            Document = document;
+        }
+
+        public void NewFile()
+        {
+            Document.FileName = "Untitled";
+            Document.FilePath = string.Empty;
+            Document.Text = string.Empty;
+        }
+
+        public void SaveFile()
+        {
+            File.WriteAllText(Document.FilePath, Document.Text);
+        }
+
+        public void SaveFileAs()
+        {
+            var sfd = new SaveFileDialog();
+            sfd.Filter = "Text File (*.txt)|*.txt";
+            if (sfd.ShowDialog() == true)
+            {
+                DockFile(sfd);
+                SaveFile();
+            }
+        }
+
+        public void OpenFile()
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Text File (*.txt)|*.txt";
+            if (ofd.ShowDialog() == true)
+            {
+                var fileName = ofd.FileName; // containing the full path of the file selected
+                Document.Text = File.ReadAllText(fileName);
+                DockFile(ofd);
+            }
+        }
+
+
+        private void DockFile<T>(T dialog) where T : FileDialog
+        {
+            Document.FilePath = dialog.FileName;
+            Document.FileName = dialog.SafeFileName;
+            Debug.WriteLine($"Dock: {Document.FilePath}, {Document.FileName}");
+        }
+    }
+}
--- Notepad/readme.md
+++ Notepad/readme.md
@@ -1,1 +1,4 @@
 The WPF Notepad project on [YouTube](https://www.youtube.com/watch?v=olBEvYKWNYI)
+
+The Stylet MVVM framework is adopted
+- No need for `RelayCommand`. Instead, use the Action mechanism provided by Stylet.

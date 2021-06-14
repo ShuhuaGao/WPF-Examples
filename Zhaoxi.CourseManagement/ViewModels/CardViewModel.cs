1f9548ccfd6077ce65d9e019b39f085cb8ec2069
Shuhua <nus.gao.shuhua@gmail.com>
2021-06-14 22:19:57 +0800

    datatemplate for card

---
+++ Zhaoxi.CourseManagement/ViewModels/CardViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LiveCharts.Wpf;
+using System.Windows.Media;
+using LiveCharts;
+
+namespace Zhaoxi.CourseManagement.ViewModels
+{
+    /// <summary>
+    /// Represent the data underlying a card UI 
+    /// https://www.bilibili.com/video/BV1Sy4y1b7FQ?p=35
+    /// </summary>
+    public class CardViewModel
+    {
+        public Brush Fill { get; set; }
+        public Brush Stroke { get; set; }
+        public double MonitorCurrentValue { get; private set; }
+        public IChartValues Values { get; private set; }
+        public string Icon { get; set; }
+        public string MonitorName { get; set; }
+
+        public CardViewModel()
+        {
+            var rnd = new Random();
+            MonitorCurrentValue = rnd.NextDouble();
+            Values = new ChartValues<double>(GenerateRandomNumbers(0.8, 30.0));
+        }
+
+        private IEnumerable<double> GenerateRandomNumbers(double min, double max, int n = 10)
+        {
+            var rnd = new Random();
+            var numbers = new List<double>();
+            for (int i = 0; i < n; i++)
+                numbers.Add(rnd.NextDouble() * (max - min) + min);
+            return numbers;
+        }
+    }
+}
--- Zhaoxi.CourseManagement/ViewModels/FirstPageViewModel.cs
+++ Zhaoxi.CourseManagement/ViewModels/FirstPageViewModel.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using LiveCharts.Wpf;
+using LiveCharts;
 using Stylet;
-using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Zhaoxi.CourseManagement.ViewModels
 {
     public class FirstPageViewModel : Screen
     {
+        public CardViewModel Card1 { get; set; }
+        public CardViewModel Card2 { get; set; }
+        public CardViewModel Card3 { get; set; }
+        public CardViewModel Card4 { get; set; }
+
+        public FirstPageViewModel()
+        {
+            // four cards for demo
+            Card1 = new CardViewModel
+            {
+                Fill = new SolidColorBrush(Colors.Aqua),
+                Stroke = new SolidColorBrush(Colors.Aquamarine),
+                MonitorName = "监控数据一",
+                Icon = "\uE84D"
+            };
+
+            Card2 = new CardViewModel
+            {
+                Fill = new SolidColorBrush(Colors.Brown),
+                Stroke = new SolidColorBrush(Colors.BlanchedAlmond),
+                MonitorName = "监控数据二",
+                Icon = "\uE854"
+            };
+            Card3 = new CardViewModel
+            {
+                Fill = new SolidColorBrush(Colors.MediumSeaGreen),
+                Stroke = new SolidColorBrush(Colors.Teal),
+                MonitorName = "监控数据三",
+                Icon = "\uE852"
+            };
+            Card4 = new CardViewModel
+            {
+                Fill = new SolidColorBrush(Colors.Olive),
+                Stroke = new SolidColorBrush(Colors.Orange),
+                MonitorName = "监控数据四",
+                Icon = "\uE84E"
+            };
+        }
     }
 }
